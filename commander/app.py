11e71a39bafd91708ac7dec5df498712a250e246
José  Galaz <jdgalazm@gmail.com>
2020-04-07 15:51:37 -0400

    test/implementation of commands as in producer/command_receiver

--- commander/app.py
+++ commander/app.py
@@ -1,14 +1,37 @@
 from aiohttp import web
+from lsst.ts import salobj
+
 
 def create_app():
+    remotes = {}
+
     app = web.Application()
 
     routes = web.RouteTableDef()
-    @routes.post('/')
-    async def hello(request):
+    @routes.post('/cmd')
+    async def start_cmd(request):
         data = await request.json()
-        return web.json_response(data)
-    
+        csc = data["csc"]
+        salindex = data["salindex"]
+        cmd_name = data["cmd"]
+        params = data["params"]
+
+        remote_name = f"{csc}.{salindex}"
+        if remote_name not in remotes:
+            remotes[remote_name] = salobj.Remote(
+                salobj.Domain(), csc, salindex)
+            await remotes[remote_name].start_task
+
+        cmd = getattr(remotes[remote_name], cmd_name)
+        cmd.set(**params)
+        cmd_result = await cmd.start(timeout=10)
+
+        return web.json_response({'ack': cmd_result.result})
 
     app.add_routes(routes)
+
+    async def on_cleanup(app):
+        for remote_name in remotes:
+            await remotes[remote_name].close()
+    app.on_cleanup.append(on_cleanup)
     return app
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,19 +1,38 @@
 from aiohttp import web
+from lsst.ts import salobj
 from commander.app import create_app
+from utils import NumpyEncoder
+import json
+
+index_gen = salobj.index_generator()
 
 
 async def test_hello(client):
-    data = {
+    # Arrange
+    # setup dds / csc
+    salobj.set_random_lsst_dds_domain()
+    index = next(index_gen)
+    csc = salobj.TestCsc(index=1, config_dir=None,
+                         initial_state=salobj.State.ENABLED)
+    await csc.start_task
+
+    # build data
+    cmd_data = csc.make_random_cmd_scalars()
+    data = json.loads(json.dumps({
         'csc': 'Test',
         'salindex': 1,
         'cmd': 'cmd_setScalars',
-        'params': {
-            'a': 1,
-            'b': 2
-        }
-    }
-    response = await client.post('/', json=data)
-    # response = await client.get('/')
+        'params': dict(cmd_data.get_vars())
+    }, cls=NumpyEncoder))
+
+    # Act
+    response = await client.post('/cmd', json=data)
+
+    # Assert status
     assert response.status == 200
+
+    # Assert content
     response_data = await response.json()
-    assert response_data == data
+    assert response_data == { 'ack': 'Done'}
+
+    await csc.close()
