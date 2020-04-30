d79064de8fe1c918d7b238dad1c35d9b1aee461b
spereirag <sebastian.pereira@inria.cl>
2020-04-30 17:19:25 -0400

    Add heartbeats subapp

--- commander/app.py
+++ commander/app.py
@@ -2,6 +2,7 @@ import json
 from aiohttp import web
 from lsst.ts import salobj
 from .commands import create_app as create_cmd_app
+from .heartbeats import create_app as create_heartbeat_app
 
 
 def create_app(*args, **kwargs):
@@ -9,5 +10,6 @@ def create_app(*args, **kwargs):
         middlewares=[web.normalize_path_middleware()])
 
     app.add_subapp('/cmd/', create_cmd_app())
+    app.add_subapp('/heartbeat/', create_heartbeat_app())
 
     return app
