bf6186cea7eb1316d54570e28aa2b5f01bfea530
Sebastián Fehlandt <sebafehlandt@gmail.com>
2020-05-19 15:46:10 -0400

    Add topic_names endpoint

--- commander/salinfo.py
+++ commander/salinfo.py
@@ -10,11 +10,12 @@ async def create_app(*args, **kwargs):
     salinfo_app = web.Application()
 
     domain = salobj.Domain()
-    available_idl_files = list(domain.idl_dir.glob('**/*.idl'))
-    names = [file.name.split('_', )[-1].replace('.idl', '')
-             for file in available_idl_files]
-    if kwargs.get('remotes_len_limit') is not None:
-        names = names[:kwargs.get('remotes_len_limit')]
+    available_idl_files = list(domain.idl_dir.glob("**/*.idl"))
+    names = [
+        file.name.split("_",)[-1].replace(".idl", "") for file in available_idl_files
+    ]
+    if kwargs.get("remotes_len_limit") is not None:
+        names = names[: kwargs.get("remotes_len_limit")]
 
     salinfos = {}
     for name in names:
@@ -24,17 +25,33 @@ async def create_app(*args, **kwargs):
         results = {
             salinfos[name].metadata.name: {
                 "sal_version": salinfos[name].metadata.sal_version,
-                "xml_version": salinfos[name].metadata.xml_version
+                "xml_version": salinfos[name].metadata.xml_version,
             }
-            for name in salinfos}
+            for name in salinfos
+        }
 
         return web.json_response(results)
-    salinfo_app.router.add_get('/metadata', get_metadata)
+
+    async def get_topic_names(request):
+        results = {
+            name: {
+                "command_names": list(salinfos[name].command_names),
+                "event_names": list(salinfos[name].event_names),
+                "telemetry_names": list(salinfos[name].telemetry_names),
+            }
+            for name in salinfos
+        }
+
+        return web.json_response(results)
+
+    salinfo_app.router.add_get("/metadata", get_metadata)
+    salinfo_app.router.add_get("/topic_names", get_topic_names)
 
     async def on_cleanup(salinfo_app):
         for name in names:
             await salinfos[name].close()
         await domain.close()
+
     salinfo_app.on_cleanup.append(on_cleanup)
 
     return salinfo_app
--- tests/test_salinfo.py
+++ tests/test_salinfo.py
@@ -11,15 +11,18 @@ index_gen = salobj.index_generator()
 
 
 async def test_metadata(client, *args, **kwargs):
+    """ Test the get metadata response."""
     salobj.set_random_lsst_dds_domain()
     async with salobj.Domain() as domain:
         domain = salobj.Domain()
-        available_idl_files = list(domain.idl_dir.glob('**/*.idl'))
-        names = [file.name.split('_', )[-1].replace('.idl', '')
-                for file in available_idl_files]
-        names = names[:conftest.REMOTES_LEN_LIMIT]
+        available_idl_files = list(domain.idl_dir.glob("**/*.idl"))
+        names = [
+            file.name.split("_",)[-1].replace(".idl", "")
+            for file in available_idl_files
+        ]
+        names = names[: conftest.REMOTES_LEN_LIMIT]
 
-        response = await client.get('/salinfo/metadata')
+        response = await client.get("/salinfo/metadata")
 
         assert response.status == 200
 
@@ -27,7 +30,36 @@ async def test_metadata(client, *args, **kwargs):
 
         for name, data in response_data.items():
             assert name in names
-            assert 'sal_version' in data
-            assert 'xml_version' in data
-            assert data['sal_version'].count('.') == 2
-            assert data['xml_version'].count('.') == 2
+            assert "sal_version" in data
+            assert "xml_version" in data
+            assert data["sal_version"].count(".") == 2
+            assert data["xml_version"].count(".") == 2
+
+
+async def test_topic_names(client, *args, **kwargs):
+    """ Test the get topic_names response."""
+    salobj.set_random_lsst_dds_domain()
+    async with salobj.Domain() as domain:
+        domain = salobj.Domain()
+        available_idl_files = list(domain.idl_dir.glob("**/*.idl"))
+        names = [
+            file.name.split("_",)[-1].replace(".idl", "")
+            for file in available_idl_files
+        ]
+        names = names[: conftest.REMOTES_LEN_LIMIT]
+
+        response = await client.get("/salinfo/topic_names")
+
+        assert response.status == 200
+
+        response_data = await response.json()
+        print("response_data: ", response_data)
+
+        for name, data in response_data.items():
+            assert name in names
+            assert "command_names" in data
+            assert "event_names" in data
+            assert "telemetry_names" in data
+            assert type(data["command_names"]) == list
+            assert type(data["event_names"]) == list
+            assert type(data["telemetry_names"]) == list
