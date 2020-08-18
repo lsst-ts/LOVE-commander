d4f2e42dda0c042f733c1263e0ca12146d5405d0
Sebastián Fehlandt <sebafehlandt@gmail.com>
2020-08-18 16:39:03 -0400

    Update the doc strings and apidocs

--- commander/commands.py
+++ commander/commands.py
@@ -1,9 +1,17 @@
+"""Define the Commands subapplication, which provides the endpoints to accept command requests."""
 from aiohttp import web
 from lsst.ts import salobj
 import json
 
 
 def create_app():
+    """Create the Commands application
+
+    Returns
+    -------
+    object
+        The application instance
+    """
     remotes = {}
 
     cmd = web.Application()
@@ -11,12 +19,17 @@ def create_app():
     async def start_cmd(request):
         data = await request.json()
         try:
-            assert 'csc' in data
-            assert 'salindex' in data
-            assert 'cmd' in data
-            assert 'params' in data
+            assert "csc" in data
+            assert "salindex" in data
+            assert "cmd" in data
+            assert "params" in data
         except AssertionError:
-            return web.json_response({"ack": f'Request must have JSON data with the following keys: csc, salindex, cmd_name, params. Received {json.dumps(data)}'}, status=400)
+            return web.json_response(
+                {
+                    "ack": f"Request must have JSON data with the following keys: csc, salindex, cmd_name, params. Received {json.dumps(data)}"
+                },
+                status=400,
+            )
 
         csc = data["csc"]
         salindex = data["salindex"]
@@ -24,8 +37,7 @@ def create_app():
         params = data["params"]
         remote_name = f"{csc}.{salindex}"
         if remote_name not in remotes:
-            remotes[remote_name] = salobj.Remote(
-                salobj.Domain(), csc, salindex)
+            remotes[remote_name] = salobj.Remote(salobj.Domain(), csc, salindex)
             await remotes[remote_name].start_task
 
         cmd = getattr(remotes[remote_name], cmd_name)
@@ -33,15 +45,16 @@ def create_app():
 
         try:
             cmd_result = await cmd.start(timeout=10)
-            return web.json_response({'ack': cmd_result.result})
+            return web.json_response({"ack": cmd_result.result})
         except salobj.AckTimeoutError:
             return web.json_response({"ack": "Command time out"}, status=504)
 
-    cmd.router.add_post('/', start_cmd)
+    cmd.router.add_post("/", start_cmd)
 
     async def on_cleanup(cmd):
         for remote_name in remotes:
             await remotes[remote_name].close()
+
     cmd.on_cleanup.append(on_cleanup)
 
     return cmd
