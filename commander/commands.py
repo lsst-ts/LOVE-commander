df6a51fcbaa5f94024beb644b7105165fe6376a2
Tiago Ribeiro <tiago.astro@gmail.com>
2020-08-27 14:21:38 -0700

    Minor improvements in `commands.py`. Make build cycle a variable in a .env file. Patch remotes so they look as being from a CSC. Add dockerfile to build commander with deployment image.

--- commander/commands.py
+++ commander/commands.py
@@ -12,11 +12,13 @@ def create_app():
     object
         The application instance
     """
+    domain = None
     remotes = {}
 
     cmd = web.Application()
 
     async def start_cmd(request):
+        nonlocal domain
         data = await request.json()
         try:
             assert "csc" in data
@@ -26,7 +28,8 @@ def create_app():
         except AssertionError:
             return web.json_response(
                 {
-                    "ack": f"Request must have JSON data with the following keys: csc, salindex, cmd_name, params. Received {json.dumps(data)}"
+                    "ack": f"Request must have JSON data with the following "
+                    "keys: csc, salindex, cmd_name, params. Received {json.dumps(data)}"
                 },
                 status=400,
             )
@@ -36,8 +39,19 @@ def create_app():
         cmd_name = data["cmd"]
         params = data["params"]
         remote_name = f"{csc}.{salindex}"
+
+        # Only create domain if it does not already exist.
+        if domain is None:
+            print("Creating salobj.Domain()")
+            domain = salobj.Domain()
+            domain.default_identity = "LOVE"
+
+        # Only create remote if it does not exist already.
         if remote_name not in remotes:
-            remotes[remote_name] = salobj.Remote(salobj.Domain(), csc, salindex)
+            print(f"Creating remote {remote_name}.")
+            # Create remote for commanding only, exclude all events and
+            # telemetry topics
+            remotes[remote_name] = salobj.Remote(domain, csc, salindex, include=[])
             await remotes[remote_name].start_task
 
         cmd = getattr(remotes[remote_name], cmd_name)
@@ -46,8 +60,13 @@ def create_app():
         try:
             cmd_result = await cmd.start(timeout=10)
             return web.json_response({"ack": cmd_result.result})
-        except salobj.AckTimeoutError:
-            return web.json_response({"ack": "Command time out"}, status=504)
+        except salobj.AckTimeoutError as e:
+            msg = (
+                "No ack received from component."
+                if e.ackcmd == salobj.SalRetCode.CMD_NOACK
+                else f"Last ack received {e.ackcmd}."
+            )
+            return web.json_response({"ack": f"Command time out. {msg}"}, status=504)
 
     cmd.router.add_post("/", start_cmd)
 
