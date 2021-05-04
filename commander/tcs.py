936674357f93ba1daf92e2e730525d0a8da67572
sebastian-aranda <aranda.sebastian@gmail.com>
2021-05-04 09:24:18 -0400

    Refactor ATCS to secure connection

--- commander/tcs.py
+++ commander/tcs.py
@@ -5,6 +5,8 @@ from aiohttp import web
 # from lsst.ts.observatory.control.auxtel import ATCS
 import lsst.ts.observatory.control.auxtel as auxtel
 
+atcs_client = None
+
 
 def create_app():
     """Create the TCS application
@@ -15,8 +17,21 @@ def create_app():
         The application instance
     """
     tcs_app = web.Application()
-    atcs = auxtel.ATCS()
-    atcs.start_task
+
+    async def connect_to_atcs_intance():
+        global atcs_client
+        try:
+            atcs_client = auxtel.ATCS()
+            await atcs_client.start_task
+        except Exception:
+            atcs_client = None
+
+    connect_to_atcs_intance()
+
+    def unavailable_atcs_client():
+        return web.json_response(
+            {"ack": f"ATCS Client could not stablish connection"}, status=400
+        )
 
     async def auxtel_command(request):
         req = await request.json()
@@ -25,7 +40,7 @@ def create_app():
         params = req["params"]
 
         try:
-            command = getattr(atcs, command_name)
+            command = getattr(atcs_client, command_name)
         except Exception:
             resp = {"ack": f"Invalid command"}
             return web.json_response(resp, status=400,)
@@ -39,10 +54,10 @@ def create_app():
     async def auxtel_docstrings(request):
         methods = [
             func
-            for func in dir(atcs)
-            if callable(getattr(atcs, func)) and not func.startswith("__")
+            for func in dir(atcs_client)
+            if callable(getattr(atcs_client, func)) and not func.startswith("__")
         ]
-        docstrings = {m: getattr(atcs, m).__doc__ for m in methods}
+        docstrings = {m: getattr(atcs_client, m).__doc__ for m in methods}
         return web.json_response((docstrings), status=200,)
 
     tcs_app.router.add_post("/aux", auxtel_command)
