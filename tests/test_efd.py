d8f42747ea9fe2412a5e9301547398382a928b1e
Sebastian Aranda <aranda.sebastian@gmail.com>
2022-04-05 16:54:11 -0400

    Add /efd/logmessages/ endpoint test

--- tests/test_efd.py
+++ tests/test_efd.py
@@ -1,6 +1,8 @@
 import asyncio
 import pandas as pd
+import random
 from unittest.mock import patch, MagicMock
+from commander.efd import MAX_EFD_LOGS_LEN
 
 
 # Patch for using MagicMock in async environments
@@ -17,14 +19,20 @@ class MockEFDClient(object):
     ):
         f = asyncio.Future()
         data = {}
-        for field in fields:
-            data[field] = {
-                pd.Timestamp("2020-03-06 21:49:41"): 0.21,
-                pd.Timestamp("2020-03-06 21:50:41"): 0.21,
-                pd.Timestamp("2020-03-06 21:51:41"): 0.21,
-                pd.Timestamp("2020-03-06 21:52:41"): 0.21,
-                pd.Timestamp("2020-03-06 21:53:41"): 0.21,
-            }
+        if not is_window:
+            for field in fields:
+                data[field] = {}
+                for i in range(int(MAX_EFD_LOGS_LEN)):
+                    data[field][pd.Timestamp(i)] = random.randint(1, 10000)
+        else:
+            for field in fields:
+                data[field] = {
+                    pd.Timestamp("2020-03-06 21:49:41"): 0.21,
+                    pd.Timestamp("2020-03-06 21:50:41"): 0.21,
+                    pd.Timestamp("2020-03-06 21:51:41"): 0.21,
+                    pd.Timestamp("2020-03-06 21:52:41"): 0.21,
+                    pd.Timestamp("2020-03-06 21:53:41"): 0.21,
+                }
 
         df = pd.DataFrame.from_dict(data)
         f.set_result(df)
@@ -102,6 +110,99 @@ async def test_efd_timeseries_with_errors(client):
 
     response_data = await response.json()
     print(response_data)
+
+    # Stop patching `efd_client`.
+    mock_efd_patcher.stop()
+
+
+async def test_efd_logmessages(client):
+    """Test the get timeseries response."""
+    # Start patching `efd_client`.
+    mock_efd_patcher = patch("lsst_efd_client.EfdClient")
+    mock_efd_client = mock_efd_patcher.start()
+    mock_efd_client.return_value = MockEFDClient()
+
+    cscs = {
+        "ATDome": {
+            0: {
+                "logevent_logMessage": [
+                    "private_rcvStamp",
+                    "level",
+                    "message",
+                    "traceback",
+                ],
+                "logevent_errorCode": [
+                    "private_rcvStamp",
+                    "errorCode",
+                    "errorReport",
+                    "traceback",
+                ],
+            },
+        },
+        "ATMCS": {
+            0: {
+                "logevent_logMessage": [
+                    "private_rcvStamp",
+                    "level",
+                    "message",
+                    "traceback",
+                ],
+                "logevent_errorCode": [
+                    "private_rcvStamp",
+                    "errorCode",
+                    "errorReport",
+                    "traceback",
+                ],
+            },
+        },
+    }
+    request_data = {
+        "efd_instance": "summit_efd",
+        "start_date": "2020-03-16T12:00:00",
+        "end_date": "2020-03-17T12:00:00",
+        "cscs": cscs,
+    }
+    response = await client.post("/efd/logmessages/", json=request_data)
+    assert response.status == 200
+
+    response_data = await response.json()
+
+    # CSC topics test
+    assert "ATDome-0-logevent_logMessage" in list(response_data.keys())
+    assert "ATDome-0-logevent_errorCode" in list(response_data.keys())
+    assert "ATMCS-0-logevent_logMessage" in list(response_data.keys())
+    assert "ATMCS-0-logevent_errorCode" in list(response_data.keys())
+
+    # Response size test
+    flattened_results = [
+        item for topic in response_data for item in response_data[topic]
+    ]
+    assert len(flattened_results) <= MAX_EFD_LOGS_LEN
+
+    # Response structure test
+    if len(response_data["ATDome-0-logevent_logMessage"]) > 0:
+        assert "private_rcvStamp" in response_data["ATDome-0-logevent_logMessage"][0]
+        assert "level" in response_data["ATDome-0-logevent_logMessage"][0]
+        assert "message" in response_data["ATDome-0-logevent_logMessage"][0]
+        assert "traceback" in response_data["ATDome-0-logevent_logMessage"][0]
+
+    if len(response_data["ATDome-0-logevent_errorCode"]) > 0:
+        assert "private_rcvStamp" in response_data["ATDome-0-logevent_errorCode"][0]
+        assert "errorCode" in response_data["ATDome-0-logevent_errorCode"][0]
+        assert "errorReport" in response_data["ATDome-0-logevent_errorCode"][0]
+        assert "traceback" in response_data["ATDome-0-logevent_errorCode"][0]
+
+    if len(response_data["ATMCS-0-logevent_logMessage"]) > 0:
+        assert "private_rcvStamp" in response_data["ATMCS-0-logevent_logMessage"][0]
+        assert "level" in response_data["ATMCS-0-logevent_logMessage"][0]
+        assert "message" in response_data["ATMCS-0-logevent_logMessage"][0]
+        assert "traceback" in response_data["ATMCS-0-logevent_logMessage"][0]
+
+    if len(response_data["ATMCS-0-logevent_errorCode"]) > 0:
+        assert "private_rcvStamp" in response_data["ATMCS-0-logevent_errorCode"][0]
+        assert "errorCode" in response_data["ATMCS-0-logevent_errorCode"][0]
+        assert "errorReport" in response_data["ATMCS-0-logevent_errorCode"][0]
+        assert "traceback" in response_data["ATMCS-0-logevent_errorCode"][0]
 
     # Stop patching `efd_client`.
     mock_efd_patcher.stop()
