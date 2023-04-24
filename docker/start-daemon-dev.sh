c13544ca6ebf12a8a0a894a85b0fb2858c8d4035
Sebastian Aranda <aranda.sebastian@gmail.com>
2023-04-24 15:07:44 -0400

    Adjust deployment files to work with new repository structure

--- docker/start-daemon-dev.sh
+++ docker/start-daemon-dev.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 source /home/saluser/.setup_dev.sh
 pip install /usr/src/love/
-export PYTHONPATH=$PYTHONPATH:/usr/src/love/python/love/commander
-adev runserver commander -p 5000
+adev runserver /usr/src/love/python/love/commander -p 5000
