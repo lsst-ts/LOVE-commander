1101da79abb161ad15ab00e9cee46dd0a06551f6
Sebastian Aranda Sanchez <aranda.sebastian@gmail.com>
2024-06-19 17:37:12 -0400

    Refactor use of the `Dockerfile-dev` image

--- docker/start-daemon-dev.sh
+++ docker/start-daemon-dev.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 source /home/saluser/.setup_dev.sh
-pip install -e /usr/src/love/
-adev runserver /usr/src/love/python/love/commander -p 5000
+adev runserver python/love/commander -p 5000
