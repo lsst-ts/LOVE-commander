33456fb7475a8c2081fe0a069727ebcac1dc37fe
Sebastian Aranda Sanchez <aranda.sebastian@gmail.com>
2024-06-19 17:46:10 -0400

    Remove unnecessary `Dockerfile-test`
    Adjust Jenkins pipeline to use `Dockerfile-dev` instead

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/saluser/.setup_dev.sh
-pytest -p no:cacheprovider -p no:pytest_session2file /usr/src/love/tests/
+pytest -p no:cacheprovider -p no:pytest_session2file tests/
