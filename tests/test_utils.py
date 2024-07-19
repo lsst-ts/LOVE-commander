9c99bdaf358a2ad7e993fb99c4ecd53569e7932f
Sebastian Aranda Sanchez <aranda.sebastian@gmail.com>
2024-07-19 11:33:14 -0400

    Add tests for the Planning Tool subapplication

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,2 +1,9 @@
 REMOTES_LEN_LIMIT = 10
 LSST_SITE = "love"
+
+
+def assert_all_objects_in_list_have_keys(keys, objects):
+    """Assert that all objects in a list have the same keys."""
+    for obj in objects:
+        for key in keys:
+            assert key in obj
